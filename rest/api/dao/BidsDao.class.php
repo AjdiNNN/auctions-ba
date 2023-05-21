9a48494c38b89acaf8d0203f7d81c3d819722f5f
aminakodzaga <amina.kodzaga@stu.ibu.edu.ba>
2023-05-21 12:57:22 +0200

    changes in bids dao

--- rest/api/dao/BidsDao.class.php
+++ rest/api/dao/BidsDao.class.php
@@ -7,12 +7,6 @@ require_once __DIR__.'/BaseDao.class.php';
   public function __construct(){
     parent::__construct("bids");
   }
-  public function get_by_name($name){
-    $name=strtolower($name);
-    $stmt = $this->conn->prepare("SELECT * FROM auctions WHERE LOWER(b_name) LIKE '%".$name."%'");
-    $stmt->execute();
-    return $stmt->fetchAll(PDO::FETCH_ASSOC);
-}
 
  }
  ?>
