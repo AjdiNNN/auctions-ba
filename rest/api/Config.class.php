bef1fb182935c3b1cb460a69600984ce0ced392c
AjdiNNN <ajdinhukic007@gmail.com>
2023-05-21 15:46:53 +0200

    config

--- rest/api/Config.class.php
+++ rest/api/Config.class.php
@@ -16,15 +16,11 @@ class Config
     }
     public static function DB_SCHEME()
     {
-        return Config::get_env("DB_SCHEME", "auctions");
+        return Config::get_env("DB_SCHEME", "keytrack-edu");
     }
     public static function DB_PORT()
     {
         return Config::get_env("DB_PORT", "3306");
-    }
-    public static function JWT_SECRET()
-    {
-        return Config::get_env("JWT_SECRET", "ezcb9s8UcF");
     }
     public static function get_env($name, $default)
     {
