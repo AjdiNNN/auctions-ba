e08b8856b2d92f2c2c3b1464f027df619016761d
AjdiNNN <ajdinhukic007@gmail.com>
2023-05-21 16:03:32 +0200

    Backend

--- rest/api/index.php
+++ rest/api/index.php
@@ -1,5 +1,4 @@
 <?php
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -8,65 +7,60 @@ use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 
 require_once __DIR__.'/../vendor/autoload.php';
-require_once __DIR__.'/services/ItemsService.php';
-require_once __DIR__.'/services/BidsService.php';
-require_once __DIR__.'/services/UserService.php';
+require_once __DIR__.'/services/ItemService.class.php';
+require_once __DIR__.'/services/BidService.class.php';
 require_once __DIR__.'/dao/UserDao.class.php';
 
 Flight::register('userDao', 'UserDao');
-Flight::register('user_service', 'UserService');
-Flight::register('items_service', 'ItemsService');
-Flight::register('bids_service', 'BidsService');
+Flight::register('itemService', 'ItemService');
+Flight::register('bidService', 'BidService');
 
-Flight::map('error', function (Exception $ex) {
+Flight::map('error', function(Exception $ex){
     // Handle error
     Flight::json(['message' => $ex->getMessage()], 500);
 });
 
 /* utility function for reading query parameters from URL */
-Flight::map('query', function ($name, $default_value = null) {
-    $request = Flight::request();
-    $query_param = @$request->query->getData()[$name];
-    $query_param = $query_param ? $query_param : $default_value;
-    return urldecode($query_param);
+Flight::map('query', function($name, $default_value = NULL){
+  $request = Flight::request();
+  $query_param = @$request->query->getData()[$name];
+  $query_param = $query_param ? $query_param : $default_value;
+  return urldecode($query_param);
 });
 
 // middleware method for login
+Flight::route('/*', function(){
+  //return TRUE;
+  //perform JWT decode
+  $path = Flight::request()->url;
+  if ($path == '/login' || $path == '/docs.json' || $path == '/register') return TRUE; // exclude login route from middleware
 
+  $headers = getallheaders();
+  if (@!$headers['Authorization']){
+    Flight::json(["message" => "Authorization is missing"], 403);
+    return FALSE;
+  }else{
+    try {
+      $decoded = (array)JWT::decode($headers['Authorization'], new Key(Config::JWT_SECRET(), 'HS256'));
+      Flight::set('user', $decoded);
+      return TRUE;
+    } catch (\Exception $e) {
+      Flight::json(["message" => "Authorization token is not valid"], 403);
+      return FALSE;
+    }
+  }
+});
 
 /* REST API documentation endpoint */
-Flight::route('/*', function () {
-    //return TRUE;
-    //perform JWT decode
-    $path = Flight::request()->url;
-    if ($path == '/login' || $path == '/docs.json' || $path == '/register') {
-        return true;
-    } // exclude login route from middleware
-
-    $headers = getallheaders();
-    if (@!$headers['Authorization']) {
-        Flight::json(["message" => "Authorization is missing"], 403);
-        return false;
-    } else {
-        try {
-            $decoded = (array)JWT::decode($headers['Authorization'], new Key(Config::JWT_SECRET(), 'HS256'));
-            Flight::set('user', $decoded);
-            return true;
-        } catch (\Exception $e) {
-            Flight::json(["message" => "Authorization token is not valid"], 403);
-            return false;
-        }
-    }
-});
-Flight::route('GET /docs.json', function () {
-    $openapi = \OpenApi\Generator::scan(['routes']);
-    header('Content-Type: application/json');
-    echo $openapi->toJson();
+Flight::route('GET /docs.json', function(){
+  $openapi = \OpenApi\Generator::scan(['routes']);
+  header('Content-Type: application/json');
+  echo $openapi->toJson();
 });
 
+require_once __DIR__.'/routes/ItemRoutes.php';
 require_once __DIR__.'/routes/UserRoutes.php';
-require_once __DIR__.'/routes/ItemsRoutes.php';
-require_once __DIR__.'/routes/BidsRoutes.php';
+require_once __DIR__.'/routes/BidRoutes.php';
 
 Flight::start();
 ?>
--- rest/api/routes/UserRoutes.php
+++ rest/api/routes/UserRoutes.php
@@ -1,8 +1,6 @@
 <?php
-
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
-
 /**
 * @OA\Post(
 *     path="/login",
@@ -11,7 +9,7 @@ use Firebase\JWT\Key;
 *     @OA\RequestBody(description="Basic user info", required=true,
 *       @OA\MediaType(mediaType="application/json",
 *    			@OA\Schema(
-*    				@OA\Property(property="email", type="string", example="ajdin@gmail.com",	description="Email"),
+*    				@OA\Property(property="email", type="string", example="dino.keco@gmail.com",	description="Email"),
 *    				@OA\Property(property="password", type="string", example="1234",	description="Password" )
 *        )
 *     )),
@@ -25,19 +23,19 @@ use Firebase\JWT\Key;
 *     )
 * )
 */
-Flight::route('POST /login', function () {
+Flight::route('POST /login', function(){
     $login = Flight::request()->data->getData();
-    $user = Flight::userDao()->checkExistsEmail($login['email']);
-    if (isset($user['id'])) {
-        if (password_verify($login['password'], $user['password'])) {
-            unset($user['password']);
-            $jwt = JWT::encode($user, Config::JWT_SECRET(), 'HS256');
-            Flight::json(['token' => $jwt]);
-        } else {
-            Flight::json(["message" => "Wrong password"], 404);
-        }
-    } else {
-        Flight::json(["message" => "User doesn't exist"], 404);
+    $user = Flight::userDao()->get_user_by_email($login['email']);
+    if (isset($user['id'])){
+      if(password_verify($login['password'],$user['password'])){
+        unset($user['password']);
+        $jwt = JWT::encode($user, Config::JWT_SECRET(), 'HS256');
+        Flight::json(['token' => $jwt]);
+      }else{
+        Flight::json(["message" => "Wrong password"], 404);
+      }
+    }else{
+      Flight::json(["message" => "User doesn't exist"], 404);
     }
 });
 /**
@@ -48,7 +46,7 @@ Flight::route('POST /login', function () {
 *     @OA\RequestBody(description="Basic user info", required=true,
 *       @OA\MediaType(mediaType="application/json",
 *    			@OA\Schema(
-*    				@OA\Property(property="email", type="string", example="ajdin@gmail.com",	description="Email"),
+*    				@OA\Property(property="email", type="string", example="dino.keco@gmail.com",	description="Email"),
 *    				@OA\Property(property="password", type="string", example="1234",	description="Password" ),
 *    				@OA\Property(property="username", type="string", example="ajdin",	description="Username"),
 *    				@OA\Property(property="firstname", type="string", example="Ajdin",	description="Firstname" ),
@@ -65,55 +63,27 @@ Flight::route('POST /login', function () {
 *     )
 * )
 */
-Flight::route('POST /register', function () {
-    $data = Flight::request()->data->getData();
-    if (Flight::userDao()->checkExistsUsername($data['username'])) {
-        Flight::json(["message" => "Username already registered"], 500);
-    } elseif (Flight::userDao()->checkExistsEmail($data['email'])) {
-        Flight::json(["message" => "Email already registered"], 500);
-    } else {
-        Flight::json(Flight::userDao()->addUser($data));
-    }
+Flight::route('POST /register', function(){
+  $data = Flight::request()->data->getData();
+  if(Flight::userDao()->get_user_by_username($data['username']))
+  {
+    Flight::json(["message" => "Username already registered"], 500);
+  }
+  else if(Flight::userDao()->get_user_by_email($data['email']))
+  {
+    Flight::json(["message" => "Email already registered"], 500);
+  }
+  else{
+    Flight::json(Flight::userDao()->addUser($data));
+  }
 });
-
-
-Flight::route('GET /User', function(){
-    Flight::json(Flight::user_service() -> get_all());
-
+/**
+ * @OA\Get(path="/user", tags={"user"}, security={{"ApiKeyAuth": {}}},
+ *         summary="Return current logged in user id. ",
+ *         @OA\Response( response=200, description="User id.")
+ * )
+ */
+Flight::route('GET /user', function(){
+  Flight::json(Flight::get('user')['id']);
 });
-
-Flight::route("GET /User_by_id", function(){
-  Flight::json(Flight::user_service()->get_by_id(Flight::request()->query['id']));
-});
-
-
-Flight::route('GET /User/@id', function($id){
-    Flight::json(Flight::user_service() -> get_by_id($id));
-
-});
-
-
-Flight::route('POST /User', function(){
-  $request = Flight::request()->data->getData();
-  Flight::json(['message' => "User added successfully",
-                'data' => Flight::user_service()->add($request)
-               ]);
-
-
-  });
-
-
-  Flight::route('PUT /User/@id', function($id){
-    $User = Flight::request()->data->getData();
-    Flight::json(['message' => "User edit successfully",
-                  'data' => Flight::user_service()->update($User, $id)
-                 ]);
-  });
-
-
-Flight::route('DELETE /User/@id', function($id){
-    Flight::user_service()->delete($id);
-    Flight::json(["message"=> "deleted"]);
-});
-
-?>;
+?>
