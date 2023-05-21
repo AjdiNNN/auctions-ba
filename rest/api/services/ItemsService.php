2cc28b9a15b800aa0a42101283d12f20df23d339
aminakodzaga <amina.kodzaga@stu.ibu.edu.ba>
2023-05-21 10:34:29 +0200

    adding services

---
+++ rest/api/services/ItemsService.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'BaseService.php';
+require_once __DIR__."/../dao/ItemsDao.class.php";
+
+class AdminService extends BaseService{
+    public function __construct(){
+        parent::__construct(new ItemsDao);
+    }
+}
+?>
